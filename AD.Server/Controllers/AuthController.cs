4f9f67d91a8a93279f37cdde2ab2d59bcc3d5962
aqys <mikkelmartinlarsen@gmail.com>
2026-06-01 11:11:06 +0200

    add AuthController with login, logout and endpoints

---
+++ AD.Server/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet("login")]
+    public IActionResult Login()
+    {
+        return Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    [HttpPost("logout")]
+    [Authorize]
+    public IActionResult Logout()
+    {
+        return SignOut(new AuthenticationProperties { RedirectUri = "/" }, 
+            CookieAuthenticationDefaults.AuthenticationScheme, 
+            OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("user")]
+    [Authorize]
+    public IActionResult GetUser()
+    {
+        return Ok(new
+        {
+            isAuthenticated = User.Identity?.IsAuthenticated ?? false,
+            username = User.Identity?.Name,
+            claims = User.Claims.Select(c => new { c.Type, c.Value })
+        });
+    }
+}
