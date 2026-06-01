c037cdd41d5dfe435c7f10efa647da237e3c58ec
aqys <mikkelmartinlarsen@gmail.com>
2026-06-01 11:15:08 +0200

    fix usings

--- AD.Server/Controllers/AuthController.cs
+++ AD.Server/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
