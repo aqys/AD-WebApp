5d55ff47a71d7e49e68494233eb5b929fa7541af
aqys <mikkelmartinlarsen@gmail.com>
2026-06-01 12:11:06 +0200

    add secret

--- AD.Server/Program.cs
+++ AD.Server/Program.cs
@@ -16,12 +16,23 @@ builder.Services.AddAuthentication(options =>
     options.ClientId = builder.Configuration["Authentication:Adfs:ClientId"];
     options.CallbackPath = builder.Configuration["Authentication:Adfs:CallbackPath"];
     options.SaveTokens = true;
+    options.ClientSecret = builder.Configuration["Authentication:Adfs:ClientSecret"];
+    options.ResponseType = "code";
     
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
         NameClaimType = "name",
         RoleClaimType = "roles"
+    };
+    // Allow extra clock skew in development to tolerate server/client time differences.
+    // Reduce or remove in production; prefer fixing system time sync instead.
+    options.TokenValidationParameters.ClockSkew = TimeSpan.FromMinutes(120);
+    
+    options.RequireHttpsMetadata = false;
+    options.BackchannelHttpHandler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
     };
 });
 
@@ -43,5 +54,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "auth successful");
 
 app.Run();
